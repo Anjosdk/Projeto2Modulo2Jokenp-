df65822c2b19395c2e765826f5a23b2f10dd5927
Anjosdk <97922555+Anjosdk@users.noreply.github.com>
2022-02-06 17:21:27 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  const resultado = getRandomInt (1 , 3 );
-console.log(resultado);
